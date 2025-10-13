daefc36b27ca5a7997bc7e05c1b3afa1e7b7fa75
Matthew Franchini <matchewf11@gmail.com>
2025-10-13 01:24:46 -0700

    got rid of mini

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -91,3 +91,24 @@ require('lazy').setup {
 --
 -- look into the lsp's and stuff i wnat to keep
 -- which langs do i wnat it for
+--
+-- return {
+--   'echasnovski/mini.nvim',
+--   version = false,
+--   event = 'VeryLazy',
+--   config = function()
+--     -- mini.ai 	Extend and create a/i textobjects
+--     -- mini.align 	Align text interactivel
+--     -- mini.comment 	Comment lines
+--     -- mini.move 	Move any selection in any direction
+--     -- mini.operators 	Text edit operators
+--     -- mini.snippets 	Manage and expand snippet
+--     -- mini.splitjoin 	Split and join argument
+--     -- mini.surround 	Surround action
+--
+--     -- mini.bracketed 	Go forward/backward with square brackets
+--     -- mini.jump 	Jump to next/previous single character
+--     -- mini.jump2d 	Jump within visible lines
+--     -- mini.sessions 	Session management
+--   end,
+-- }
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,6 +1,5 @@
 return {
   { import = 'plugins.mason' },
-  { import = 'plugins.misc' },
   { import = 'plugins.tools' },
   { import = 'plugins.visuals' },
   { import = 'plugins.editing' },
