91f59ee62c9c1fb73e94ff81f4cedc0287362a4b
Matthew Franchini <matchewf11@gmail.com>
2025-10-16 16:15:25 -0700

    renamed colorscheme.lua

---
+++ nvim/lua/plugins/colors.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    'rebelot/kanagawa.nvim',
+    name = 'kanagawa',
+    priority = 1000,
+    config = function()
+      require('kanagawa').setup {
+        commentStyle = { italic = false },
+      }
+      vim.cmd.colorscheme 'kanagawa-dragon'
+    end,
+  },
+}
