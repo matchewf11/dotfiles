f863a89d99cdbc7f8ee44496f9090f33e1405a68
Matthew Franchini <matchewf11@gmail.com>
2025-12-05 01:19:05 -0800

    fixed spacing for lua

---
+++ nvim/after/ftplugin/lua.lua
@@ -0,0 +1,13 @@
+-- Lua Specific Config
+
+-- spaces for tabs
+vim.opt_local.expandtab = true
+
+-- what to auto-indent
+vim.opt_local.shiftwidth = 2
+
+-- spaces that tabs count for
+vim.opt_local.tabstop = 2
+
+-- how many spaces <Tab> inserts
+vim.opt_local.softtabstop = 2
--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -1,7 +1,7 @@
 return {
 	{
-		"ellisonleao/gruvbox.nvim",
-		priority = 1000,
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
 		opts = { contrast = "hard" },
 	},
 	{
