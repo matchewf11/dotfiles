1a099ab98e7bf9077ed4bddf61c10c201f20e47f
Matthew Franchini <matchewf11@gmail.com>
2025-12-04 18:14:44 -0800

    styling and added .luarc.json for project

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -1,56 +1,56 @@
 return {
 	{
-		'ellisonleao/gruvbox.nvim',
-		priority = 1000 ,
-		opts = { contrast = 'hard' }
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		opts = { contrast = "hard" },
 	},
-  {
-    'saghen/blink.cmp',
-    version = '1.*',
-		event = 'InsertEnter',
-    dependencies = {
-      'rafamadriz/friendly-snippets',
-      { 'L3MON4D3/LuaSnip', version = 'v2.*' },
-    },
-    opts = {
-      snippets = { preset = 'luasnip' },
-      signature = { enabled = true },
-    },
-  },
+	{
+		"saghen/blink.cmp",
+		version = "1.*",
+		event = "InsertEnter",
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			{ "L3MON4D3/LuaSnip", version = "v2.*" },
+		},
+		opts = {
+			snippets = { preset = "luasnip" },
+			signature = { enabled = true },
+		},
+	},
 	{
 		"nvim-treesitter/nvim-treesitter",
-		branch = 'master',
+		branch = "master",
 		lazy = false,
-		build = ":TSUpdate"
+		build = ":TSUpdate",
 	},
 	{
-		'neovim/nvim-lspconfig',
-		event = { 'BufReadPre', 'BufNewFile' },
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
 		config = function()
-			for _, server in ipairs {
-				'lua_ls', -- pacman
-			} do
+			for _, server in ipairs({
+				"lua_ls", -- pacman
+			}) do
 				vim.lsp.enable(server)
 			end
-		end
+		end,
 	},
 	{
-		'stevearc/oil.nvim',
+		"stevearc/oil.nvim",
 		opts = { delete_to_trash = true },
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		lazy = false,
-		keys = { {'<leader>e', '<CMD>Oil<CR>', desc = 'Open Oil'} },
+		keys = { { "<leader>e", "<CMD>Oil<CR>", desc = "Open Oil" } },
 	},
 	{
-		'ibhagwan/fzf-lua',
+		"ibhagwan/fzf-lua",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		opts = {},
 		keys = {
-			{ '<leader>sb', '<CMD>FzfLua buffers<CR>', desc = 'Search Buffers' },
-			{ '<leader>sf', '<CMD>FzfLua files<CR>', desc = 'Search Files' },
-			{ '<leader>so', '<CMD>FzfLua oldfiles<CR>', desc = 'Search Oldfiles' },
-			{ '<leader>sl', '<CMD>FzfLua blines<CR>', desc = 'Search BLines' },
+			{ "<leader>sb", "<CMD>FzfLua buffers<CR>", desc = "Search Buffers" },
+			{ "<leader>sf", "<CMD>FzfLua files<CR>", desc = "Search Files" },
+			{ "<leader>so", "<CMD>FzfLua oldfiles<CR>", desc = "Search Oldfiles" },
+			{ "<leader>sl", "<CMD>FzfLua blines<CR>", desc = "Search BLines" },
 		},
-		cmd = { 'FzfLua' },
+		cmd = { "FzfLua" },
 	},
 }
