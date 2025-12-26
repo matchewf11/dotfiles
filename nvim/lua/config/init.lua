5403aa903fc734bbaf307b2d95f0d9deb895b0fd
Matthew Franchini <matchewf11@gmail.com>
2025-12-26 13:23:24 -0800

    foo

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -56,6 +56,20 @@ vim.keymap.set('n', '<leader>d', vim.diagnostic.setloclist, { desc = 'Diagnostic
 -- { nvim-dap  (nvim-dap-ui), (nvim-nio), (nvim-dap-go) }
 -- {'folke/which-key.nvim' }
 -- { mini.nvim } stuff (surround, ai)
+-- { mbbill/undotree }
+-- { harpoon }
+-- {vim-fugitive}
+-- guess-indent
+-- todo comments
+-- mini.ai
+-- mini.surround
+-- {neotest, nvim-nio,fixcursorhold,neotest-golang,nvim-dap-go}
+-- {nvim-dap, nvim-dap-ui,nvim-nio}
+-- vim.pack
+-- toggle term
+-- comment.vim
+-- vim-fugitive
+-- which-key
 
 -- :so to source this file
 -- :map jk
@@ -180,36 +194,16 @@ vim.cmd.colorscheme 'gruvbox'
 --
 -- vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
 
--- { mbbill/undotree }
--- { harpoon }
--- {vim-fugitive}
--- {neotest, nvim-nio,fixcursorhold,neotest-golang,nvim-dap-go}
--- {nvim-dap, nvim-dap-ui,nvim-nio}
 --
 -- ]b to move to next buffer
 --
 -- other tpope plugins
 --
 --
---guess-indent
---todo comments
---mini.ai
---mini.surround
---other kickstart stuff
---other mini stuff
+-- other kickstart stuff
+-- other mini stuff
 -- tabs, windows, splits, tmux, i3
 -- add back kickstart.debug w/ config
--- make no changes to plugin config options or features
--- config all plugins installed in lazy
--- <https://github.com/SylvanFranklin/.config/tree/main/nvim>
--- <https://github.com/Melkeydev/NvimConfig>
--- <https://github.com/ThePrimeagen/init.lua/tree/master/lua/theprimeagen/lazy>
--- vim.pack
--- toggle term
--- comment.vim
---
--- vim-fugitive
--- which-key
 --
 -- My config ------------------------------------------------
 -- local function config()
@@ -505,4 +499,3 @@ vim.cmd.colorscheme 'gruvbox'
 --   --   'ntpeters/vim-better-whitespace',
 --   --   lazy = true,
 --   -- },
--- }
