ebab1f81d53d3082bbb1973bf33d51329746660b
Matthew Franchini <matchewf11@gmail.com>
2025-12-30 18:24:19 -0800

    mini diff

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -13,6 +13,25 @@ return {
 
       require('mini.icons').setup()
 
+      -- -- Apply hunks inside a visual/operator region
+      --  apply = 'gh', (ghip)
+      --  -- Reset hunks inside a visual/operator region
+      --  reset = 'gH',
+      --  -- Hunk range textobject to be used inside operator
+      --  -- Works also in Visual mode if mapping differs from apply and reset
+      --  textobject = 'gh', (dgh)
+      --  -- Go to hunk range in corresponding direction
+      --  goto_first = '[H',
+      --  goto_prev = '[h',
+      --  goto_next = ']h',
+      --  goto_last = ']H',
+      require('mini.diff').setup {}
+
+      vim.keymap.set('n', '<leader>h', MiniDiff.toggle_overlay, {
+        desc = 'Toggle Diff Overlay',
+      })
+
+
       require('mini.statusline').setup {
         content = {
           active = function()
@@ -70,65 +89,6 @@ return {
     end,
   },
   {
-    'lewis6991/gitsigns.nvim',
-    event = { 'BufReadPre', 'BufNewFile' },
-    opts = {
-      on_attach = function(bufnr)
-        local gitsigns = require 'gitsigns'
-
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        map('n', ']c', function()
-          if vim.wo.diff then
-            vim.cmd.normal { ']c', bang = true }
-          else
-            gitsigns.nav_hunk 'next'
-          end
-        end)
-
-        map('n', '[c', function()
-          if vim.wo.diff then
-            vim.cmd.normal { '[c', bang = true }
-          else
-            gitsigns.nav_hunk 'prev'
-          end
-        end)
-
-        map('n', '<leader>hs', gitsigns.stage_hunk)
-        map('n', '<leader>hr', gitsigns.reset_hunk)
-        map('v', '<leader>hs', function()
-          gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end)
-        map('v', '<leader>hr', function()
-          gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end)
-
-        map('n', '<leader>hS', gitsigns.stage_buffer)
-        map('n', '<leader>hR', gitsigns.reset_buffer)
-        map('n', '<leader>hp', gitsigns.preview_hunk)
-        map('n', '<leader>hi', gitsigns.preview_hunk_inline)
-        map('n', '<leader>hb', function()
-          gitsigns.blame_line { full = true }
-        end)
-        map('n', '<leader>hd', gitsigns.diffthis)
-        map('n', '<leader>hD', function()
-          gitsigns.diffthis '~'
-        end)
-        map('n', '<leader>hQ', function()
-          gitsigns.setqflist 'all'
-        end)
-        map('n', '<leader>hq', gitsigns.setqflist)
-        map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
-        map('n', '<leader>tw', gitsigns.toggle_word_diff)
-        map({ 'o', 'x' }, 'ih', gitsigns.select_hunk)
-      end,
-    },
-  },
-  {
     'stevearc/conform.nvim',
     cmd = { 'ConformInfo' },
     keys = {
@@ -184,7 +144,7 @@ return {
     event = { 'BufReadPost', 'BufNewFile' },
     keys = {
       {
-        '[C',
+        '[c',
         function()
           require('treesitter-context').go_to_context(vim.v.count1)
         end,
@@ -371,7 +331,6 @@ complete_bline 	complete line (current buffer only)
 -- mini.pairs 	Autopairs
 -- mini.snippets 	Manage and expand snippets
 -- mini.splitjoin 	Split and join arguments
--- mini.surround 	Surround actions
 --
 -- mini.basics 	Common configuration presets
 -- mini.bracketed 	Go forward/backward with square brackets
@@ -379,7 +338,6 @@ complete_bline 	complete line (current buffer only)
 -- mini.clue 	Show next key clues
 -- mini.cmdline 	Command line tweaks
 -- mini.deps 	Plugin manager
--- mini.diff 	Work with diff hunks
 -- mini.extra 	Extra 'mini.nvim' functionality
 -- mini.files 	Navigate and manipulate file system
 -- mini.git 	Git integration
