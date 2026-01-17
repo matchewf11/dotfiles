e83e783f275280cd54fe218b050d29952095b490
Matthew Franchini <matchewf11@gmail.com>
2026-01-16 21:23:08 -0800

    feat: updated cargo keybinds

--- nvim/after/ftplugin/rust.lua
+++ nvim/after/ftplugin/rust.lua
@@ -1,2 +1,3 @@
 vim.keymap.set('n', '<localleader>r', '<CMD>!cargo run<CR>', { desc = 'Cargo Run' })
 vim.keymap.set('n', '<localleader>c', '<CMD>!cargo check<CR>', { desc = 'Cargo Check' })
+vim.keymap.set('n', '<localleader>t', '<CMD>!cargo test<CR>', { desc = 'Cargo Test' })
