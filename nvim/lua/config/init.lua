d9a1d7d46aba2ecda5747367d8b3dab5964b8ebe
Matthew Franchini <matchewf11@gmail.com>
2026-01-19 00:48:16 -0800

    foo

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -305,6 +305,51 @@ MiniDeps.later(function()
   end, 'Format Buffer')
 end)
 
+--   event = { 'BufReadPre', 'BufNewFile' },
+MiniDeps.now(function()
+  MiniDeps.add {
+    source = 'neovim/nvim-lspconfig',
+  }
+  for _, server in ipairs {
+    'lua_ls', -- pacman
+    'gopls', -- go
+    'clangd', -- pacman
+    'ts_ls', -- npm
+    'rust_analyzer', -- rustup
+    'taplo', -- cargo
+    'hls', -- ghcup
+    'pyright', -- npm
+    -- 'sqls', -- go
+    'bashls', -- pacman
+  } do
+    vim.lsp.enable(server)
+  end
+end)
+
+--   event = { 'BufReadPost', 'BufNewFile' },
+MiniDeps.later(function()
+  MiniPick.add {
+    source = 'mfussenegger/nvim-lint',
+  }
+  local lint = require 'lint'
+  lint.linters_by_ft = {
+    lua = { 'luacheck' }, -- luarocks
+    c = { 'clangtidy' }, -- pacman
+    sh = { 'shellcheck' }, -- pacman
+    -- sql = { 'sqlfluff' }, -- pipx
+    go = { 'golangcilint' }, -- pacman
+    make = { 'checkmake' }, -- go
+    rust = { 'clippy' }, -- cargo
+    markdown = { 'markdownlint' }, -- npm
+  }
+  vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+    callback = function()
+      lint.try_lint()
+      -- lint.try_lint 'cspell' -- npm
+    end,
+  })
+end)
+
 -- MiniDeps.add {
 --   source = '',
 --   checkout = '',
